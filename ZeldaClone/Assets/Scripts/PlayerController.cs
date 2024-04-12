b1c97722defff18782ea61e07b11ceb5d905403b
FernandoPestillo <fernandopestillo@gmail.com>
2024-04-12 00:01:49 -0300

    Movimentação Player + Organização de pastas

---
+++ ZeldaClone/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D _playerRigidBody2D;
+    public float        _playerSpeed;
+    private Vector2     _playerDirection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _playerDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+     void FixedUpdate()
+    {
+        _playerRigidBody2D.MovePosition(_playerRigidBody2D.position + _playerDirection * _playerSpeed * Time.fixedDeltaTime);
+    }
+}
