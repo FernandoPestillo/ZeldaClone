59fa819799166618d5408be203b99ff82ee41b89
PedroGardim04 <136342141+PedroGardim04@users.noreply.github.com>
2024-04-28 12:06:06 -0300

    Adição PauseMenu

---
+++ ZeldaClone/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform pauseMenu;
+    public object Imput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenu.gameObject.activeSelf) 
+            { 
+                pauseMenu.gameObject.SetActive(false);
+                Time.timeScale = 1;            }
+            else 
+            {
+                pauseMenu.gameObject.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }        
+    }
+    public void ResumeGame()
+    {
+        pauseMenu.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
